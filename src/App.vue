ea6348adc122ce815328c5fdc6ec284f1c7cc32b
GabrieleGrossi <grossigabriele98@gmail.com>
2023-06-14 16:35:35 +0200

    Add a random Jumbotron for bonus

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import AppTitle from './components/AppTitle.vue';
+import Jumbo from './components/Jumbo.vue';
 </script>
 
 <template>
   <div>
     <AppTitle />
+    <Jumbo />
   </div>
 </template>
 
---
+++ src/components/Jumbo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <img src="/jumbo.png" alt="Random Jumbotrone">
+    </div>
+</template>
+
+<script>
+    export default{
+        data() {
+            return{
+
+            }
+        },
+        methods:{
+        }
+    }
+</script>
+
+<style>
+    img{
+        width: 800px;
+    }
+</style>

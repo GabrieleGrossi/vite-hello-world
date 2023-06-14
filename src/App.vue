2ed31e1d42e3edc46f2e866d591355a4f4bc44f5
GabrieleGrossi <grossigabriele98@gmail.com>
2023-06-14 16:15:28 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AppTitle from './components/AppTitle.vue';
+</script>
+
+<template>
+  <div>
+    <AppTitle />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>
+            La mia prima app con Vite
+        </h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        data() {
+            return{
+
+            }
+        },
+        methods:{
+        }
+    }
+</script>
+
+<style>
+
+</style>
